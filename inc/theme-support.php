324aa54753bb778d6b8348f27f183840eb71e337
Amélie Husson <amelie@ameliehusson.com>
2012-08-08 13:03:44 +0200

    Enhances theme supports and added stylesheet for rtl

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -11,34 +11,69 @@ if (!function_exists('waht_theme_support')
 ):
     /**
      * Theme support
+     * See http://codex.wordpress.org/Function_Reference/add_theme_support
      */
     function waht_theme_support()
     {
-        // add support for custom header
-        add_theme_support('custom-header', array(
-            'default-image'          => get_template_directory_uri() . '/assets/img/logo.png',
-            'random-default'         => false,
-            'width'                  => 245,
-            'height'                 => 145,
-            'flex-height'            => true,
-            'flex-width'             => true,
-            'default-text-color'     => '',
-            'header-text'            => false,
-            'uploads'                => true,
-            'wp-head-callback'       => '',
-            'admin-head-callback'    => '',
-            'admin-preview-callback' => '',
-        ));
+        global $wp_version;
 
-        // add support for custom backgrounds
-        add_theme_support('custom-background', array(
-            'default-color' => '#ffffff',
-            'default-image' => get_template_directory_uri() .
-                "/assets/img/white_paper_texture_background_seamless_pattern.jpg"
-        ));
+        if (version_compare($wp_version, '3.4', '>='))
+            // Add support for custom header
+            // See http://codex.wordpress.org/Custom_Headers
+            add_theme_support('custom-header', array(
+                'default-image'          => get_template_directory_uri() . '/assets/img/logo.png',
+                'random-default'         => false,
+                'width'                  => 245,
+                'height'                 => 145,
+                'flex-height'            => true,
+                'flex-width'             => true,
+                'default-text-color'     => '',
+                'header-text'            => false,
+                'uploads'                => true,
+                'wp-head-callback'       => '',
+                'admin-head-callback'    => '',
+                'admin-preview-callback' => '',
+            ));
 
-        // add support for WP menus
-        add_theme_support('menus');
+        if (version_compare($wp_version, '3.4', '>='))
+            // Add support for custom backgrounds
+            // See http://codex.wordpress.org/Custom_Backgrounds
+            add_theme_support('custom-background', array(
+                'default-color'          => '#ffffff',
+                'default-image'          => get_template_directory_uri() .
+                    "/assets/img/white_paper_texture_background_seamless_pattern.jpg",
+                'wp_head_callback'       => 'custom_background_cb', // todo
+                'admin-head-callback'    => '', // todo
+                'admin-preview-callback' => '' // todo
+            ));
+
+        // Add support for WP menus
+        if (version_compare($wp_version, '3.0', '>='))
+            add_theme_support('menus');
+
+        if (version_compare($wp_version, '3.1', '>='))
+            // Add support for custom post format
+            // See http://codex.wordpress.org/Post_Formats
+            add_theme_support('post-formats', array(
+                'aside',
+                'gallery',
+                'link',
+                'image',
+                'quote',
+                'status',
+                'video',
+                'audio',
+                'chat'
+            ));
+
+        if (version_compare($wp_version, '2.9', '>='))
+            // Add support for custom post thumbnails
+            // See http://codex.wordpress.org/Post_Thumbnails
+            add_theme_support('post-thumbnails');
+
+        if (version_compare($wp_version, '3.0', '>='))
+            // add support for automatic feed links
+            add_theme_support('automatic-feed-links');
     }
 endif;
 add_action('after_setup_theme', 'waht_theme_support');
--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
 /**
- * @description:    This is the main template file
- * @name:           index.php
- * @package:        waht
- * @author:         Amélie Husson
- * @uri:            https://github.com/Othella/waht
+ * @description: Main template file
+ * @name       : index.php
+ * @package    : waht
+ * @author     : Amélie Husson
+ * @uri        : https://github.com/Othella/waht
+ *
+ * TODO Code index.php
  */
+?>
+<?php get_header(); ?>
+<div id="content" class="<?php echo CONTAINER_CLASSES; ?>">
+    <?php waht_main_before(); ?>
+    <section id="main" role="main" class="<?php echo MAIN_CLASSES; ?>">
+        <?php get_template_part('loop'); ?>
+    </section>
+    <!-- /#main -->
+    <?php waht_main_after(); ?>
+    <?php waht_sidebar_before(); ?>
+    <aside id="complementary" class="<?php echo SIDEBAR_CLASSES; ?>">
 
-get_header();
+    </aside>
+    <!-- /#compementary -->
+    <?php waht_sidebar_after(); ?>
+</div><!-- /#content -->
 
-get_footer();
+<?php get_footer();
