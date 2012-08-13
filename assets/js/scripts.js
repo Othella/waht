3f040fb4ea5903b2448329166becf4ce3e1a6727
Amélie Husson <amelie@ameliehusson.com>
2012-08-13 12:06:43 +0200

    Dynamic body position (depending on bootstrap top-fixed navbar)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,21 +4,48 @@
  * Package: waht
  * URI:     https://github.com/Othella/waht
  */
-jQuery('document').ready(function ($) {
+jQuery(function ($) {
 
-    /* Use bootstrap styled list */
-    var widgets = $('#page-wrap')
-        .find('.widget_archive, .widget_categories, .widget_links, .widget_meta, .widget_pages, .widget_recent_entries, .widget_recent_comments, .widget_rss, .widget_nav_menu');
-    widgets.addClass('nav');
-    widgets.find('.widgettitle').addClass('nav-header');
-    widgets.find('ul').addClass('nav nav-list');
+    /**
+     *  Use bootstrap styled list in widget
+     */
+    function wahtBootstrapStyle() {
+        var widgets = $('#page-wrap')
+            .find('.widget_archive, .widget_categories, .widget_links, .widget_meta, .widget_pages, .widget_recent_entries, .widget_recent_comments, .widget_rss, .widget_nav_menu');
+        widgets.addClass('nav');
+        widgets.find('.widgettitle').addClass('nav-header');
+        widgets.find('ul').addClass('nav nav-list');
+    }
 
-    var addToHomeConfig = {
-        animationIn: 'bubble',
-        animationOut:'drop',
-        lifespan:    10000,
-        expire:      2,
-        touchIcon:   true
-    };
+    /**
+     * Improve page layout if using bootstrap's top navbar
+     */
+    function wahtPositionBody() {
+        var navbar = $('nav.navbar-fixed-top'),
+            body = $('body.waht-navbar-fixed-top');
+        body.css('padding-top', navbar.height());
+    }
+
+    $('document').ready(function () {
+
+        /**
+         * Configure Add To Homescreen plugin
+         * Documentation: http://cubiq.org/add-to-home-screen
+         */
+        var addToHomeConfig = {
+            animationIn: 'bubble',
+            animationOut:'drop',
+            lifespan:    10000,
+            expire:      2,
+            touchIcon:   true
+        };
+
+        wahtBootstrapStyle();
+        wahtPositionBody();
+    });
+
+    $(window).load(function () {
+        $(window).resize(wahtPositionBody);
+    });
 
 });
