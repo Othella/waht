b3ccdaec0a616b8bee1c55712c05c9bcfb54f64d
Amélie Husson <amelie@ameliehusson.com>
2012-08-14 17:50:19 +0200

    Added new hooks for loops

--- inc/hooks.php
+++ inc/hooks.php
@@ -9,49 +9,75 @@
 
 /**
  * Custom links and tags -
- * @used in header.php
+ * @used_in header.php
  */
 function waht_head() { do_action('waht_head'); }
 
 /**
  * Page header before hook
- * @used in header.php
+ * @used_in header.php
  */
 function waht_page_header_before() { do_action('waht_page_header_before'); }
 
 /**
  * Page header after hook
- * @used in header.php
+ * @used_in header.php
  */
 function waht_page_header_after() { do_action('waht_page_header_after'); }
 
 /**
  * Page wrap before hook
- * @used in header.php
+ * @used_in header.php
  */
 function waht_page_wrap_before() { do_action('waht_page_wrap_before'); }
 
 /**
  * Main section before hook
+ * @used_in
  */
 function waht_main_before() { do_action('waht_main_before'); }
 
 /**
  * Main section after hook
+ * @used_in
  */
 function waht_main_after() { do_action('waht_main_after'); }
 
 /**
  * Loop before hook
- * @used in loop.php
+ * @used_in loop.php, loop-single.php, loop-page.php
  */
 function waht_loop_before() { do_action('waht_loop_before'); }
 
 /**
  * Loop after hook
- * @used in loop.php
+ * @used in loop.php, loop-single.php, loop-page.php
  */
 function waht_loop_after() { do_action('waht_loop_after'); }
+
+/**
+ * Post before hook
+ * @used_in loop.php, loop-single.php, loop-page.php
+ */
+function waht_post_before() { do_action('waht_post_before'); }
+
+/**
+ * Post inside after hook
+ * @used in loop.php, loop-single.php, loop-page.php
+ */
+function waht_post_inside_after() { do_action('waht_post_inside_after'); }
+
+/**
+ * Post inside before hook
+ * @used_in loop.php, loop-single.php, loop-page.php
+ */
+function waht_post_inside_before() { do_action('waht_post_inside_before'); }
+
+/**
+ * Post after hook
+ * @used in loop.php, loop-single.php, loop-page.php
+ */
+function waht_post_after() { do_action('waht_post_after'); }
 
 /**
  * Sidebar aside before hook
@@ -75,6 +101,11 @@ function waht_page_wrap_after() { do_action('waht_page_wrap_after'); }
  */
 function waht_page_footer_before() { do_action('waht_page_footer_before'); }
 
+/**
+ * Page footer inside hook
+ * @used in footer.php
+ */
+function waht_page_footer_inside() { do_action('waht_page_footer_inside'); }
 /**
  * Page footer after hook
  * @used in footer.php
