10be63e1bd3f256ed574b4e3cb37d605363e8a20
Amélie Husson <amelie@ameliehusson.com>
2012-08-19 17:15:07 +0200

    waht-3 Code archive.php

--- archive.php
+++ archive.php
@@ -8,5 +8,18 @@
  * @author     : Amélie Husson (http://ameliehusson.com)
  * @uri        : https://github.com/Othella/waht
  */
+?>
+<?php get_header(); ?>
+<div id="content" class="<?php echo CONTAINER_CLASSES; ?>">
 
-// TODO (a.h) Code archive.php
+    <?php waht_main_before(); ?>
+    <section id="main" role="main" class="<?php echo MAIN_CLASSES; ?>">
+
+        <?php get_template_part('loop'); ?>
+    </section>
+    <!-- /#main -->
+    <?php waht_main_after(); ?>
+    <?php get_sidebar(); ?>
+</div><!-- /#content -->
+
+<?php get_footer(); ?>
