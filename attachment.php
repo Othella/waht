2c29dca2604658cdd9698e99268ef22dc3430ac0
Amélie Husson <amelie@ameliehusson.com>
2012-09-03 14:02:35 +0200

    waht-4 Code attachment.php

--- attachment.php
+++ attachment.php
@@ -6,5 +6,16 @@
  * @author     : Amélie Husson (http://ameliehusson.com)
  * @uri        : https://github.com/Othella/waht
  */
+?>
+<?php get_header(); ?>
+<div id="content" class="<?php echo CONTAINER_CLASSES; ?>">
+	<?php waht_main_before(); ?>
+    <section id="main" role="main" class="<?php echo MAIN_CLASSES; ?>">
+		<?php get_template_part('loop', 'attachment'); ?>
+    </section>
+    <!-- /#main -->
+	<?php waht_main_after(); ?>
+	<?php get_sidebar(); ?>
+</div><!-- /#content -->
 
-// TODO (a.h) Code attachment.php
+<?php get_footer();
---
+++ loop-attachment.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @description: Attachment loop template. Used when viewing a single attachment.
+ * @name       : ${FILE_NAME}
+ * @package    : waht
+ * @author     : Amélie Husson (http://ameliehusson.com)
+ * @uri        : https://github.com/Othella/waht
+ */
+global $multipage, $numpages, $page;
+?>
+<?php waht_loop_before(); ?>
+<?php while (have_posts()) : the_post(); ?>
+<?php waht_post_before(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class('attachment'); ?>>
+	<?php waht_post_inside_before(); ?>
+    <header class="post-header">
+        <h1 class="post-title">
+			<?php the_title(); ?>
+			<?php if ($multipage) : ?>
+            <span class="page-number">(<?php printf(esc_attr('Page %s of %s', 'waht'), $page, $numpages); ?>)</span>
+			<?php endif ?>
+        </h1>
+    </header>
+    <section class="post-content">
+		<?php get_template_part('content', get_post_type()) ?>
+    </section>
+    <footer class="post-footer">
+		<?php waht_link_pages(); ?>
+		<?php waht_meta(); ?>
+    </footer>
+	<?php comments_template(); ?>
+	<?php waht_post_inside_after(); ?>
+</article>
+<?php waht_post_after(); ?>
+<?php endwhile; ?>
+<?php waht_loop_after();
