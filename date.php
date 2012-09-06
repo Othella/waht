4f6f5ec083f6915cdf848f93ddf027c4b08702c7
Amélie Husson <amelie@ameliehusson.com>
2012-09-06 10:30:26 +0200

    Corrected typo

--- date.php
+++ date.php
@@ -20,7 +20,7 @@
 				<?php if (is_day()) : ?>
 				<?php printf(__('<span>%s</span> Daily Archive', 'waht'), get_the_date()); ?>
 				<?php elseif (is_month()) : ?>
-				<?php printf(__('<span>%s</span> Monthly Archive', 'waht'), get_the_date(_x('F Y', 'monthly archives date format', 'twentyten'))); ?>
+				<?php printf(__('<span>%s</span> Monthly Archive', 'waht'), get_the_date(_x('F Y', 'monthly archives date format', 'waht'))); ?>
 				<?php
 			elseif (is_year()) : ?>
 				<?php printf(__('<span>%s</span> Yearly Archives', 'waht'), get_the_date(_x('Y', 'yearly archives date format', 'waht'))); ?>
